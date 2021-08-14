00b327524001f34c49cb3b04725bf95cdecea08e
Rounak Talreja <72248204+Rounak0901@users.noreply.github.com>
2021-08-14 15:17:53 +0530

    initial commit

---
+++ FoodRunner/app/src/main/java/com/internshala/foodrunner/activity/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.internshala.foodrunner.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.internshala.foodrunner.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
---
+++ FoodRunner/app/src/main/java/com/internshala/foodrunner/model/RestaurantMenu.kt
@@ -0,0 +1,8 @@
+package com.internshala.foodrunner.model
+
+
+data class RestaurantMenu (
+        var id:String,
+        var name:String,
+        var cost_for_one:String
+)
